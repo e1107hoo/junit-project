bf23d47c0d81c024b730776dc7585a74db814b07
e1107hoo <ooe@e-mind.co.jp>
2013-07-05 17:45:13 +0900

    StringUtilsTestの作成
    テスト駆動開発にそった手法で行うため

---
+++ src/test/java/chapter18/topc1/StringUtilsTest.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package chapter18.topc1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+/**
+ * @author mind
+ *
+ */
+public class StringUtilsTest {
+	
+	@Test
+	public void「aaa」を入力すると「aaa」を取得できる() throws Exception {
+		assertThat(actual, is("aaa"));
+	}
+	
+}
