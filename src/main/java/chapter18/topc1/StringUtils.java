ee40066bdca521b830b81c9e9917671878166329
e1107hoo <ooe@e-mind.co.jp>
2013-07-05 17:55:40 +0900

    StringUtils.javaの実装
    テスト駆動開発の仮実装

---
+++ src/main/java/chapter18/topc1/StringUtils.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package chapter18.topc1;
+
+/**
+ * @author mind
+ *
+ */
+public class StringUtils {
+	
+	/**
+	 * @param string
+	 * @return
+	 */
+	public String toSnakeCase(String string) {
+		// TODO 自動生成されたメソッド・スタブ
+		return "aaa";
+	}
+	
+}
