4d16a1d79eed2bf1395702a4fa1d0ca18c1a2ffb
e1107hoo <ooe@e-mind.co.jp>
2013-07-10 17:18:37 +0900

    Calculator.javaの作成
    Calculator.javaを仮実装しテストをgreenにした

---
+++ src/main/java/chapter18/topc2/Calculator.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package chapter18.topc2;
+
+/**
+ * @author mind
+ *
+ */
+public class Calculator {
+	
+	/**
+	 * @param i
+	 * @param j
+	 */
+	public int divide(int i, int j) {
+		return 0;
+		
+	}
+	
+}
