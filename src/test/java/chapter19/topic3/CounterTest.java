0fbd0e366a8f56eb3ff42bf558dea5ebfc35f3f0
e1107hoo <ooe@e-mind.co.jp>
2013-07-12 17:30:57 +0900

    二個目のテストケースを作成
    パターン2個目のテストを作成テストは赤

--- src/test/java/chapter19/topic3/CounterTest.java
+++ src/test/java/chapter19/topic3/CounterTest.java
@@ -7,16 +7,33 @@ import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
 
 /**
  * @author mind
  *
  */
+@RunWith(Enclosed.class)
 public class CounterTest {
 	
-	@Test
-	public void incrementメソッドを実行すると1が取得できる() {
-		Counter sut = new Counter();
-		assertThat(sut.increment(), is(1));
+	public static class 初期状態の場合 {
+		
+		@Test
+		public void incrementメソッドを実行すると1が取得できる() {
+			Counter sut = new Counter();
+			assertThat(sut.increment(), is(1));
+		}
+	}
+	
+	public static class 一回実行状態 {
+		
+		@Test
+		public void incrementメソッドを1回実行した状態でincrementメソッドを実行すると2が取得できる() {
+			Counter sut = new Counter();
+			sut.increment();
+			assertThat(sut.increment(), is(2));
+			
+		}
 	}
 }
