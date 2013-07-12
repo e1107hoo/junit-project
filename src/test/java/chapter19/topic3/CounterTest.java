19995bcc96398c428d1aec1c1aa6f232e5ce8220
e1107hoo <ooe@e-mind.co.jp>
2013-07-12 17:37:33 +0900

    CounterTest.javaをリファクタリング
    @Beforeを作成しテストを見やすくした。

--- src/test/java/chapter19/topic3/CounterTest.java
+++ src/test/java/chapter19/topic3/CounterTest.java
@@ -6,6 +6,7 @@ package chapter19.topic3;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
@@ -19,19 +20,33 @@ public class CounterTest {
 	
 	public static class 初期状態の場合 {
 		
+		Counter	sut;
+		
+		
+		@Before
+		public void setUp() {
+			sut = new Counter();
+		}
+		
 		@Test
 		public void incrementメソッドを実行すると1が取得できる() {
-			Counter sut = new Counter();
 			assertThat(sut.increment(), is(1));
 		}
 	}
 	
 	public static class 一回実行状態 {
 		
+		Counter	sut;
+		
+		
+		@Before
+		public void setUp() {
+			sut = new Counter();
+			sut.increment();
+		}
+		
 		@Test
 		public void incrementメソッドを1回実行した状態でincrementメソッドを実行すると2が取得できる() {
-			Counter sut = new Counter();
-			sut.increment();
 			assertThat(sut.increment(), is(2));
 			
 		}
