ea04f602355eaa6a3467f3f8df8aa991371a67fa
e1107hoo <ooe@e-mind.co.jp>
2013-07-16 17:19:43 +0900

    NumberUtilsとisEvenの作成仮実装
    isEvenでtrueを返して最速でのテストグリーンを実現

---
+++ src/main/java/chapter18/topic4/NumberUtils.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package chapter18.topic4;
+
+/**
+ * @author mind
+ *
+ */
+public class NumberUtils {
+	
+	/**
+	 * @param i
+	 * @return
+	 */
+	public boolean isEven(int i) {
+		// TODO 自動生成されたメソッド・スタブ
+		return true;
+	}
+	
+}
