d8ae5b600efc8abc7ecc64315cd8d5293a1eef7f
e1107hoo <ooe@e-mind.co.jp>
2013-07-16 17:28:25 +0900

    メソッドのリファクタリング
    リファクタリングを実装することでプログラムを見やすく

--- src/main/java/chapter18/topic4/NumberUtils.java
+++ src/main/java/chapter18/topic4/NumberUtils.java
@@ -9,8 +9,8 @@ package chapter18.topic4;
  */
 public class NumberUtils {
 	
-	public boolean isEven(int i) {
-		return i % 2 == 0 ? true : false;
+	public boolean isEven(int num) {
+		return num % 2 == 0 ? true : false;
 	}
 	
 }
