ab96fd6a9919e73ed668497398f9fa1bc7c50c2d
e1107hoo <ooe@e-mind.co.jp>
2013-07-22 17:38:48 +0900

    二個目のテスト追加
    @beforeで初期化をまとめた

--- src/test/java/chapter18/topic5/ItemStockTest.java
+++ src/test/java/chapter18/topic5/ItemStockTest.java
@@ -6,17 +6,34 @@ package chapter18.topic5;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class ItemStockTest {
 	
+	ItemStock	sut;
+	Item		book;
+	
+	
+	@Before
+	public void setUp() {
+		sut = new ItemStock();
+		book = new Item("book", 3000);
+	}
+	
 	@Test
 	public void 初期状態でgetNumから0が取得できる() {
-		ItemStock sut = new ItemStock();
-		Item book = new Item("book", 3000);
 		
 		assertThat(sut.getNum(book), is(0));
 		
+	}
+	
+	@Test
+	public void 初期状態でaddでItemを追加するとgetNumで1が取得できる() {
+		
+		sut.add(book);
+		
+		assertThat(sut.getNum(book), is(1));
 	}
 	
 }
