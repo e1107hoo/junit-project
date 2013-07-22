878754cc54f6f6dc54650a1ecbbea78fa148102f
e1107hoo <ooe@e-mind.co.jp>
2013-07-22 17:54:11 +0900

    三つ目のテストケース追加
    三つ目のテストケースを追加

--- src/test/java/chapter18/topic5/ItemStockTest.java
+++ src/test/java/chapter18/topic5/ItemStockTest.java
@@ -8,32 +8,58 @@ import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
 
+@RunWith(Enclosed.class)
 public class ItemStockTest {
 	
-	ItemStock	sut;
-	Item		book;
-	
-	
-	@Before
-	public void setUp() {
-		sut = new ItemStock();
-		book = new Item("book", 3000);
+	public static class 初期状態の場合 {
+		
+		ItemStock	sut;
+		Item		book;
+		
+		
+		@Before
+		public void setUp() {
+			sut = new ItemStock();
+			book = new Item("book", 3000);
+		}
+		
+		@Test
+		public void 初期状態でgetNumから0が取得できる() {
+			
+			assertThat(sut.getNum(book), is(0));
+			
+		}
+		
+		@Test
+		public void 初期状態でaddでItemを追加するとgetNumで1が取得できる() {
+			
+			sut.add(book);
+			
+			assertThat(sut.getNum(book), is(1));
+		}
 	}
 	
-	@Test
-	public void 初期状態でgetNumから0が取得できる() {
+	public static class bookが1回追加されている場合 {
 		
-		assertThat(sut.getNum(book), is(0));
+		ItemStock	sut;
+		Item		book;
 		
-	}
-	
-	@Test
-	public void 初期状態でaddでItemを追加するとgetNumで1が取得できる() {
 		
-		sut.add(book);
+		@Before
+		public void setUp() {
+			sut = new ItemStock();
+			book = new Item("book", 3000);
+			sut.add(book);
+		}
 		
-		assertThat(sut.getNum(book), is(1));
+		@Test
+		public void Itemが1つ追加されている状態でgetNumで1が取得できる() {
+			assertThat(sut.getNum(book), is(1));
+		}
+		
 	}
 	
 }
